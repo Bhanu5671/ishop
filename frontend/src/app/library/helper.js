167f7b86107747b322d5f02b22c31389bac37d7c
bhanu5671 <bhanujangid5671@gmail.com>
2026-01-31 18:24:32 +0530

    getcookies function modifies

--- frontend/src/app/library/helper.js
+++ frontend/src/app/library/helper.js
@@ -11,10 +11,13 @@ function titletoSlug(title) {
 }
 
 const getCookie = (name) => {
-  const value = `; ${document?.cookie}`;
+  if (typeof window === "undefined") return null;
+
+  const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop().split(';').shift();
   return null;
-}
+};
+
 
 export { titletoSlug, axiosApiInstance, getCookie }
